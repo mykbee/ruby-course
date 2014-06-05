1f9514071441bb01ab342c12a884210bd14cc92f
Michael Bearden <bearden.michael@gmail.com>
2014-06-05 12:32:40 -0500

    Tests pass for bar and menu_items

--- bar.rb
+++ bar.rb
@@ -1,4 +1,23 @@
 require 'time' # you're gonna need it
 
 class Bar
+  attr_reader :name, :menu_items
+
+  def initialize(name)
+    @name = name
+    @menu_items = []
+  end
+
+  def add_menu_item(name, price)
+    menu_items << MenuItem.new(name, price)
+  end
+end
+
+class MenuItem
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
 end
