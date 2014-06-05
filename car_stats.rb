bc3719f9773555a0602f5520eb897257f6c70694
Michael Bearden <bearden.michael@gmail.com>
2014-06-05 11:18:30 -0500

    Initial commit, car stats v1

--- car_stats.rb
+++ car_stats.rb
@@ -1,3 +1,33 @@
 class CarStats
-  # TODO
+  def self.calc_top_color(cars)
+    colors = cars.map { |car| car.color }
+    histogram = {}
+    colors.each do |c|
+      if histogram[c].nil?
+        histogram[c] = 1
+      else
+        histogram[c] += 1
+      end
+    end
+    histogram.max_by { |color, count| count}.first
+  end
+
+  def self.calc_bottom_color(cars)
+    colors = cars.map { |car| car.color }
+    histogram = {}
+    colors.each do |c|
+      if histogram[c].nil?
+        histogram[c] = 1
+      else
+        histogram[c] += 1
+      end
+    end
+    histogram.min_by { |color, count| count}.first
+  end
 end
+
+
+class CarStats
+  def self.calc_top_color(cars)
+    colors = cars.map { |c| c.color }
+    histogram = colors.inject(Hash.new(0)) do 
