743c13f3a2188e1335f0ebd3efc4d814fcac8308
Michael Bearden <bearden.michael@gmail.com>
2014-06-05 12:39:02 -0500

    Added happy hour default discount zero

--- bar.rb
+++ bar.rb
@@ -6,10 +6,15 @@ class Bar
   def initialize(name)
     @name = name
     @menu_items = []
+    @happy_discount = 0
   end
 
   def add_menu_item(name, price)
     menu_items << MenuItem.new(name, price)
+  end
+
+  def happy_discount(discount=0)
+    @happy_discount
   end
 end
 
